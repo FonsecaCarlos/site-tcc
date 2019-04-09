0cc411e410f16d468dab6bae94f005e7331f7ed6
Carlos Fonseca <foncecacarlos@hotmail.com>
2019-04-08 22:38:47 -0400

    Adição da rota da paginação

--- backend/src/api/narrativeText/narrativeTextService.js
+++ backend/src/api/narrativeText/narrativeTextService.js
@@ -5,4 +5,17 @@ NarrativeText.methods(['get', 'post', 'put', 'delete'])
 NarrativeText.updateOptions({new: true, runValidators: true})
 NarrativeText.after('post', errorHandler).after('put', errorHandler)
 
+const index = (req, res, next) => {
+    const { page=1 } = req.query
+    NarrativeText.paginate({}, { page, limit:15 })
+        .then((narrativeText) => {
+            return res.json(narrativeText)
+        })
+        .catch((errors) => {
+            return res.json(errors)
+        })
+}
+
+NarrativeText.route('index', ['get'], index)
+
 module.exports = NarrativeText
