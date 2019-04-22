6c67f344344a2acd43f0598c69573623896ba676
Carlos Fonseca <foncecacarlos@hotmail.com>
2019-04-22 19:40:00 -0400

    adição da rota /narrativeText para o ReadHistory

--- frontend/src/main/routes.js
+++ frontend/src/main/routes.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 import { Switch, Route, Redirect } from 'react-router'
 
-import mainHistory from '../pages/manageHistory/main'
-//import BillingCycle from '../billingCycle/billingCycle'
+import mainHistory from '../pages/manageHistory'
+import ReadHistory from '../pages/readHistory'
 
 export default props => (
     <div className='content-wrapper'>
         <Switch>
             <Route exact path='/' component={mainHistory} />
-            {/*<Route path='billingCycles' component={BillingCycle} />*/}
+            <Route path='/narrativeText' component={ReadHistory} />
             <Redirect from='*' to='/' />
         </Switch>
     </div>
