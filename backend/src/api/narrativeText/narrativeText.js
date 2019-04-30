2c91fffde3e2e2034a331d815a52c877ad7d83f1
Carlos Fonseca <foncecacarlos@hotmail.com>
2019-04-30 10:32:33 -0400

    adicionado plugin paginate aggregate e modificações no Modelo
    No modelo foram adicionados os campos isMaster e sharedWith

--- backend/src/api/narrativeText/narrativeText.js
+++ backend/src/api/narrativeText/narrativeText.js
@@ -1,18 +1,23 @@
 const restful = require('node-restful')
 const mongoose = restful.mongoose
-const mongoosePaginate = require('mongoose-paginate');
+const mongoosePaginate = require('mongoose-paginate')
+const mongooseAggregatePaginate = require('mongoose-aggregate-paginate')
+const ObjectId = mongoose.Schema.Types.ObjectId
 
 const NarrativeTextSchema = new mongoose.Schema({
-    author: { type: String /*Author*/, required: true },
+    author: { type: ObjectId, required: [true, 'Id do autor não informado!'] },
     createdAt: { type: Date, default: Date.now },
     title: { type: String, required: [true, 'Informe o título do texto!'] },
-    text: { type: String, required: [true, 'Texto vazio!'] },
+    text: { type: String, required: false, default:'' },
     status: { type: String, default: 'ABERTO',required: true, uppercase: true,
         enum: ['CONCLUIDO', 'ABERTO'] },
     isPublic: { type: Boolean, default: true},
-    alternativeText: { type: [String], required: false }
+    isMaster: { type: Boolean, default: true },
+    alternativeText: { type: [ObjectId], required: false },
+    sharedWith: { type: [ObjectId], required: false }
 })
 
 NarrativeTextSchema.plugin(mongoosePaginate)
+NarrativeTextSchema.plugin(mongooseAggregatePaginate)
 
 module.exports = restful.model('NarrativeText', NarrativeTextSchema)
