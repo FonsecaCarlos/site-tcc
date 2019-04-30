f71a82be2fabfbb197af457a6715cca1342ba9f1
Carlos Fonseca <foncecacarlos@hotmail.com>
2019-04-30 10:35:20 -0400

    adição de novos serviços

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,6 @@
-import axios from 'axios';
+import axios from 'axios'
+import consts from '../consts'
 
-const api = axios.create({ baseURL: 'http://localhost:3003/api' });
+const api = axios.create({ baseURL: consts.API_URL })
 
-export default api;
+export default api
